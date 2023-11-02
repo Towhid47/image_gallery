03ae0aeee954350343cfa81f08836081d9208123
Towhid47 <mdtowhidurrahman47@gmail.com>
2023-11-02 23:07:18 +0600

    Hover Effect, input checkbox, React Icons added

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Card = (props) => {
 
-    const {id , image} = props.IMAGE
+    const {id , image} = props.product
 
     return (
-        <div className='text-center'>
+        <div className='text-center relative hover:cursor-pointer'>
+            
+            <div className=' w-full h-full hover:bg-black opacity-30 p-4 absolute z-10  transition hover:duration-300'>
+              <label className='absolute left-2'>
+                   <input type="checkbox" className='accent-black' />
+              </label>
+            </div>
+           
+
             <img src={image} alt="" className='lg:w-full'/>
         </div>
     );
--- src/Components/CardsContainer/CardsContainer.jsx
+++ src/Components/CardsContainer/CardsContainer.jsx
@@ -1,17 +1,27 @@
 import IMAGES from '../../images.json';
 import Card from '../Card/Card';
 
+import { FaImages } from 'react-icons/fa';
+
 const CardsContainer = () => {
+
+    let products = [...IMAGES] ;
+    
+    
+
+
     return (
         <div className='mt-0 lg:mt-4'>
 
-             <ul className='grid grid-cols-2 md:grid-cols-5  mx-auto [&>*:first-child]:col-span-2   [&>*:first-child]:row-span-2 gap-1 lg:gap-5 '> 
+            
+
+             <ul className='grid grid-cols-2 md:grid-cols-5  mx-auto [&>*:first-child]:col-span-2  [&>*:first-child]:row-span-2 gap-1 lg:gap-5 '> 
 
                    {
-                     IMAGES.map((IMAGE)=> <li className="border-2 border-gray-400 rounded-md  lg:w-full"><Card IMAGE = {IMAGE}  key={IMAGE.id}></Card></li>) 
+                     products.map((product)=> <li className="border-2 border-gray-300 rounded-md  lg:w-full"><Card product = {product}  key={product.id}></Card></li>) 
                    }
 
-                   <li className='border-2 border-gray-400 border-dashed rounded-md'>Add Image</li>
+                   <li className='border-2 border-gray-400 border-dashed rounded-md flex justify-center items-center'>Add Image &nbsp; <FaImages className='text-2xl'/></li>
              </ul>
 
         </div>
